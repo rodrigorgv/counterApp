e2989a7289f612127640711bb294b7cd03ebb428
rodrigorgv <rodrigo.emilianogv@gmail.com>
2024-09-05 21:22:17 -0600

    se finaliza counter app con iconos integrados

--- src/presentation/screens/CounterM3Screen.tsx
+++ src/presentation/screens/CounterM3Screen.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react'
 import { Platform, Pressable, SectionList, StyleSheet, Text, View } from 'react-native'
 import { PrimaryButton } from '../components';
-import { Button, FAB } from 'react-native-paper';
+import  { FAB }  from 'react-native-paper';
 import { globalStyles } from '../theme/global.styles';
 import Icon from 'react-native-vector-icons/Ionicons';
 
@@ -15,13 +15,15 @@ export const CounterM3Screen = () => {
       <Text>Hola</Text>
       <Icon name="accessibility-outline" size={25}></Icon>
       <FAB
-        label='+1'
         style={globalStyles.fab}
         onPress={() => setCount(count + 1)}
         onLongPress={() => setCount(0)}
+        icon="add"
       />
     </View>
   )
 }
+
+export default CounterM3Screen
 
 
